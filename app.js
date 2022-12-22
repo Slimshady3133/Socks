ddaadcbfe9e52a39378680e19ab8730a4b3d0db3
Farit <faritahmetshin@gmail.com>
2022-12-22 14:13:58 +0300

    Change main fail

--- app.js
+++ app.js
@@ -3,15 +3,44 @@ require('@babel/register');
 require('dotenv').config();
 
 const express = require('express');
-
-const { sequelize } = require('./db/models');
+const morgan = require('morgan');
+const db = require('./db/models');
 
 const app = express();
+const path = require('path');
+const session = require('express-session');
+
+const errorHandler = require('./middlewaare/errorHandler');
+const ssr = require('./middlewaare/ssr');
+
+const sessionCongig = require('./config/sessions');
+
+const sequelize = require('./db/models');
+const UserRoute = require('./routes/user');
+
+app.use(session(sessionCongig));
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(errorHandler);
+app.use(ssr);
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 const PORT = process.env.PORT ?? 3000;
 
-sequelize.authenticate();
+app.use('/auth', UserRoute);
 
-app.listen(PORT, () => {
-  console.log(`*** Server started at ${PORT} port ***`);
-});
+const start = async () => {
+  try {
+    await db.sequelize.authenticate();
+    await db.sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`*** Server started at ${PORT} port ***`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+start();
