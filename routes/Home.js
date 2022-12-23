430ec138ebf0739e667eaa98d2b2e86dbb54c63d
Slimshady3133 <i.puzakov2010@mail.ru>
2022-12-23 15:06:35 +0300

    Home Router

---
+++ routes/Home.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const Home = require('../views/Home');
+
+router.get('/', (req, res) => {
+  res.renderComponent(Home);
+});
+
+module.exports = router;
