c2e0c778e872bfa6055b34f0bb9aa26bbae7fcb6
Slimshady3133 <i.puzakov2010@mail.ru>
2022-12-23 15:16:32 +0300

    delete

--- routes/mod.js
+++
@@ -1,9 +0,0 @@
-const homeRouter = require('express').Router();
-
-const HomePage = require('../views/HomePage');
-
-homeRouter.get('/', (req, res) => {
-  res.renderComponent(HomePage);
-});
-
-module.exports = homeRouter;
